fd8e6fa05bdc5698a3603be4f6b041b276b1b459
NickPaferov <paferov.nikolai@gmail.com>
2023-04-24 18:43:14 +0300

    refactoring off App component (add scroll)

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import {StyleSheet, View} from 'react-native';
+import {ScrollView, StyleSheet} from 'react-native';
 import {Main} from "./src/app/Main";
 import {store} from "./src/app/store";
 import {Provider} from "react-redux";
@@ -7,16 +7,15 @@ import React from "react";
 export default function App() {
     return (
         <Provider store={store}>
-            <View style={styles.container}>
+            <ScrollView style={styles.container}>
                 <Main/>
-            </View>
+            </ScrollView>
         </Provider>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        paddingTop: 50,
         flex: 1,
         backgroundColor: "#fff",
     },
